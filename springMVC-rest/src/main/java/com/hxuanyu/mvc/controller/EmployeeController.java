8939edf253a562e0d90e6456a5d805a21bece06f
hanxuanyu <2252193204@qq.com>
2021-09-08 22:22:48 +0800

    restful案例

--- springMVC-rest/src/main/java/com/hxuanyu/mvc/controller/EmployeeController.java
+++ springMVC-rest/src/main/java/com/hxuanyu/mvc/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.hxuanyu.mvc.controller;
 
+import com.hxuanyu.mvc.bean.Employee;
+import com.hxuanyu.mvc.dao.EmployeeDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 /**
- * TODO
+ * 员工控制器
  * date 2021/9/8 18:54
  *
  * @author hanxuanyu
@@ -12,8 +18,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class EmployeeController {
-    @RequestMapping("/")
-    public String index() {
+
+    private final EmployeeDao employeeDao;
+
+    @Autowired
+    public EmployeeController(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployee(Model model) {
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList", employeeList);
+        return "employee_list";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable Integer id) {
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String addEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
+    public String getEmployeeById(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("employee", employee);
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/employee";
+    }
+
+    @RequestMapping("/test")
+    public String testRedirect(){
         return "index";
     }
 }
+
